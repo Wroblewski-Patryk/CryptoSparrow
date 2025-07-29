f9f6998c57d83c79976ccf9202e9a5a529beb8a5
Patryk Wróblewski <wroblewskipatryk@gmail.com>
2025-07-29 20:31:05 +0200

    feat(ui): custom theme & fonts, header logo, cleanup

--- apps/client/src/ui/layout/dashboard/Header.tsx
+++ apps/client/src/ui/layout/dashboard/Header.tsx
@@ -1,12 +1,16 @@
+'use client';
 import Link from 'next/link';
+import { useAuth } from '../../../context/AuthContext';
 import { LuLayoutDashboard, LuLogOut } from 'react-icons/lu';
 
 export default function Header() {
-  
+  const { user, loading, logout } = useAuth();
+
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900 shadow-md">
       <div className="container mx-auto flex justify-between items-center px-6 py-4">
-        <Link href="/" className="text-xl font-bold text-gray-900 dark:text-white">
+        <Link href="/" className="text-xl font-headline font-bold text-gray-900 dark:text-white">
+          <img src="/logo.png" alt="Logotype - CryptoSparrow" className="h-8 w-8 mr-2 float-left" />
           CryptoSparrow
         </Link>
         <nav>
@@ -21,13 +25,13 @@ export default function Header() {
               </Link>
             </li>
             <li>
-              <Link
-                href="/auth/logout"
+              <button
+                onClick={logout}
                 className="text-sm text-gray-100 hover:underline"
-              >
+                >
                 <LuLogOut className="inline mr-2 mb-1" />
                 Wyloguj
-              </Link>
+              </button>
             </li>
           </ul>
         </nav>
--- apps/client/src/ui/layout/public/Footer.tsx
+++ apps/client/src/ui/layout/public/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="text-center text-sm text-gray-500 dark:text-gray-400 py-6 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900">
+    <footer className=" text-center text-sm text-gray-500 dark:text-gray-400 py-6 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900">
       © {new Date().getFullYear()} CryptoSparrow. Wszelkie prawa zastrzeżone.
     </footer>
   );
-}
+} 
